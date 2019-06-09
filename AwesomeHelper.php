41f77ad1e61c79ac7ecfd2553271a4ae65516cd6
Andrey <r.o.profile@gmail.com>
2019-06-09 22:13:19 +0500

    Add files via upload

---
+++ AwesomeHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace common\helpers;
+
+class AwesomeHelper
+{
+
+    public static function getWords(string $string, int $getWordsAmount = 12): string
+    {
+        $words = explode(' ', $string);
+        $returndedWords = array_slice($words, 0, $getWordsAmount);
+
+        return implode(' ', $returndedWords);
+    }
+
+    public static function toCamelCase(string $string): string
+    {
+        $words = explode('_', $string);
+        $capitalWords = array_map(function ($item) {
+            return ucfirst($item);
+        }, $words);
+
+        return implode('', $capitalWords);
+    }
+
+    public static function transliterate(string $string): string
+    {
+        $cyr = [
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
+            'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
+            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П',
+            'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'
+        ];
+        $lat = [
+            'a', 'b', 'v', 'g', 'd', 'e', 'io', 'zh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p',
+            'r', 's', 't', 'u', 'f', 'h', 'ts', 'ch', 'sh', 'sht', 'a', 'i', 'y', 'e', 'yu', 'ya',
+            'A', 'B', 'V', 'G', 'D', 'E', 'Io', 'Zh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P',
+            'R', 'S', 'T', 'U', 'F', 'H', 'Ts', 'Ch', 'Sh', 'Sht', 'A', 'I', 'Y', 'e', 'Yu', 'Ya'
+        ];
+
+        return str_replace($cyr, $lat, $string);
+    }
+}
